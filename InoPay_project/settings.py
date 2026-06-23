d26fd535834726c0abe121d9b97489b42328309f
Diego Lozano <diegolozanodev@gmail.com>
2026-06-23 00:26:04 -0500

    Add Procfile, render.yaml, and runtime.txt for deployment configuration; update SECRET_KEY handling in settings.py

--- InoPay_project/settings.py
+++ InoPay_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_#kb$cw*fxf=e9(-ncrh44fw_1_^jjguir3943)j@ga$z&c+60'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-_#kb$cw*fxf=e9(-ncrh44fw_1_^jjguir3943)j@ga$z&c+60')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
